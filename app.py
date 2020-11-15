9ceb36e56e0fcb1478d3990b4157af1c2304cc2f
1341007936 <1341007936@qq.com>
2020-11-15 18:58:03 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,11 +1,26 @@
+import os               #http://127.0.0.1:5000
+import sys
+import click
 from flask import Flask,render_template
 from flask import url_for
-app = Flask(__name__)       #http://127.0.0.1:5000
+from flask_sqlalchemy import SQLAlchemy
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
+
+app = Flask(__name__)
+db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 @app.route('/home')
 @app.route('/')
 def index():
-    return render_template('index.html',name=name,movies=movies)
+    user = User.quert.first()
+    movies = Movie.query.all()
+    return render_template('index.html',user = user,movies=movies)
 @app.route('/user/<name>')
 def user_page(name):
     return 'User: %s' % name
@@ -17,22 +32,47 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for', num=2))
     return 'Test page'
-name = 'dengkw'
-movies = [
-    {'title': 'My Neighbor Totoro', 'year':'1988'},
-    {'title': 'Dead Poets Society', 'year':'1989'},
-    {'title': 'A Perfect World', 'year': '1993'},
-    {'title': 'Leon', 'year': '1994'},
-    {'title': 'Mahjong', 'year': '1996'},
-    {'title': 'Swallowtail Butterfly', 'year': '1996'},
-    {'title': 'King of Comedy', 'year': '1999'},
-    {'title': 'Devils on the Doorstep', 'year': '1999'},
-    {'title': 'WALL-E', 'year': '2008'},
-    {'title': 'The Pork of Music', 'year': '2012'},
-]
+@app.cli.command()
+@click.option('--drop',is_flag=True,help='Create after frop.')
+    # 设置选项
+def initdb(drop):
+    """Iiitialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
 
-
-
+    name = 'dengkw'
+    movies = [
+        {'title': 'My Neighbor Totoro', 'year':'1988'},
+        {'title': 'Dead Poets Society', 'year':'1989'},
+        {'title': 'A Perfect World', 'year': '1993'},
+        {'title': 'Leon', 'year': '1994'},
+        {'title': 'Mahjong', 'year': '1996'},
+        {'title': 'Swallowtail Butterfly', 'year': '1996'},
+        {'title': 'King of Comedy', 'year': '1999'},
+        {'title': 'Devils on the Doorstep', 'year': '1999'},
+        {'title': 'WALL-E', 'year': '2008'},
+        {'title': 'The Pork of Music', 'year': '2012'},
+    ]
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'],year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.column(db.String(20))
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
 
 
 
